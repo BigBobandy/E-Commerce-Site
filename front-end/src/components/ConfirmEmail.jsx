723c91174ebe1f3a6b7b631e7fff3e5d933b1bd3
BigBobandy <jdykes207@gmail.com>
2023-06-02 23:18:03 -0400

    Update ConfirmEmail.jsx
    ConfirmEmail component's purpose is to handle the email confirmation process for a user. This component uses several hooks from react and react-router-dom to manage local state, side effects, and navigation.
    
    Upon rendering, this component fetches the email confirmation code provided in the URL using the useParams hook from react-router-dom. It then initiates a request to the server using the useEffect hook to confirm the user's email.
    
    A mutable reference, emailConfirmedRef, is used to track whether the confirmation process was successful. If it was, a success message is set using the setMessage function and the user is redirected to the home page after 3 seconds.
    
    In the event the confirmation fails, the component allows the user to manually trigger the confirmation process. The showModal state controls the visibility of a ConfirmationModal component which pops up when the "Click to manually confirm" button is clicked.
    
    In the returned JSX, a welcome message is displayed along with the state of the confirmation process. The "Click to manually confirm" button is only visible if the email confirmation was not successful. The ConfirmationModal component is shown based on the showModal state. The onClose prop is passed to it, which sets showModal to false when the modal is closed, thus hiding it.
    
    Overall, the ConfirmEmail component is responsible for validating a user's email upon account creation, providing feedback to the user, and handling possible exceptions by offering a manual confirmation alternative.

--- front-end/src/components/ConfirmEmail.jsx
+++ front-end/src/components/ConfirmEmail.jsx
@@ -1,25 +1,75 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 
 function ConfirmEmail() {
   const navigate = useNavigate();
+  // Accessing the email confirmation code parameter with the useParams hook from react-router
+  const { codeParam } = useParams();
+  // state to handle the message to be displayed
+  const [message, setMessage] = useState("");
+  // state to track whether or not the confirmation was successful
+  const emailConfirmedRef = useRef(false); // using a mutable reference here to track confirmation
+  // state for a popup that will let the user confirm their email
+  const [showModal, setShowModal] = useState(false);
 
-  useEffect(
-    () => {
-      // After 3 seconds the user gets redirected to the home page
-      setTimeout(() => {
-        navigate("/");
-      }, 3000);
-    },
-    { navigate }
-  );
+  // This hook is responsible for sending a GET request to the server when the component mounts
+  useEffect(() => {
+    // This async function will send a GET request to the server to confirm the user's email
+    const confirmEmail = async () => {
+      // Only make the request if we haven't successfully confirmed the email during this component's rendering yet
+      if (!emailConfirmedRef.current) {
+        const response = await fetch(
+          `http://localhost:3000/confirm/${codeParam}`
+        );
+
+        if (response.ok) {
+          // If the email was confirmed successfully, update the message and
+          // then redirect the user to the home page after 3 seconds.
+          setMessage(
+            "Thanks for confirming your email! You will be redirected to the home page shortly."
+          );
+
+          emailConfirmedRef.current = true; // set the ref's current value to true
+
+          // Send the user to the home page after 3 seconds
+          setTimeout(() => {
+            navigate("/");
+          }, 3000);
+
+          // If the email couldn't be confirmed with the initial GET request for some reason
+          // Allow user to manually enter the code for confirmation in the ConfirmationModal
+        } else {
+          setMessage(
+            "There was an error confirming your email. Please click below to try again."
+          );
+        }
+      }
+    };
+
+    // calling the confirmEmail function to send the GET request
+    confirmEmail();
+
+    // return function to useEffect which is called when component unmounts.
+    return () => {};
+  }, [codeParam, navigate]);
 
   return (
     <div>
-      <h2>
-        Thanks for confirming your email! You will be redirected to the home
-        page shortly
-      </h2>
+      <h1>Welcome!</h1>
+      <h2>{message}</h2>
+      {!emailConfirmedRef.current && (
+        <button
+          className="manual-confirm-btn"
+          onClick={() => setShowModal(true)}
+        >
+          Click to manually confirm
+        </button>
+      )}
+      {showModal && (
+        <ConfirmationModal
+          onClose={() => {
+            setShowModal(false);
+          }}
+        />
+      )}
     </div>
   );
 }
