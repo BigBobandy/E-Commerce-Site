82d5f82916a1f415ebde7235514e9604edb9efd6
BigBobandy <jdykes207@gmail.com>
2023-06-02 23:11:28 -0400

    Changed Modal Functionality
    Removed Email Prop: I removed the email prop from the ConfirmationModal component, as it is no longer necessary for the email confirmation process.
    
    Confirmation Code Handling: Updated the handleEmailConfirmation function. The function no longer requires the email in the body of the POST request. Now, it only sends the confirmationCode to the server, aligning with the updated backend logic.
    
    Error and Message Handling: Included state handling for errors and messages to ensure user feedback is properly delivered. It sets error messages if the server response is not OK or if there's any exception while processing the request. Conversely, it sets success messages if the response is OK.
    
    Loading State: Incorporated a loading state to provide visual feedback to the user during the email confirmation process. It displays a loading spinner while the confirmation request is in progress.
    
    Redirection: After successful email confirmation, the modal closes and navigates the user to the home page. The duration for closing the modal has been adjusted from 2 seconds to 5 seconds, giving users sufficient time to read the success message.
    
    State Updates in Input Field: Included state updates in the input field to keep track of the confirmationCode input by the user.

--- front-end/src/components/ConfirmationModal.jsx
+++ front-end/src/components/ConfirmationModal.jsx
@@ -2,47 +2,61 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./styles/ConfirmationModal.css";
 
-function ConfirmationModal({ email, onClose }) {
+function ConfirmationModal({ onClose }) {
+  // State to hold the value of the input field for the confirmation code
   const [confirmationCode, setConfirmationCode] = useState("");
+
+  // State to hold any error messages
   const [error, setError] = useState(null);
+
+  // State to hold success message
   const [message, setMessage] = useState(null);
+
+  // State to control loading spinner display
   const [loading, setLoading] = useState(false);
 
+  // Instantiating the navigate function from react-router-dom for redirection after confirmation
   const navigate = useNavigate();
 
+  // Function to handle email confirmation, triggered on form submission
   const handleEmailConfirmation = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevent default form submission
 
     // Clears any previous errors/messages
     setError(null);
     setMessage(null);
+
     setLoading(true); // Start loading
 
+    // Try to confirm the email with the provided code
     try {
       const response = await fetch("http://localhost:3000/confirm", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email, code: confirmationCode }),
+        body: JSON.stringify({ confirmationCode: confirmationCode }), // Send confirmation code as request body
       });
 
       const data = await response.json();
 
+      // If response is okay, show success message and redirect to home page after 3 seconds
       if (response.ok) {
         setMessage("Congratulations! Your email is now confirmed.");
         setTimeout(() => {
-          onClose();
-          navigate("/"); // Navigate to the home page after confirmation
-        }, 2000); // Close the modal after 2 seconds
+          onClose(); // Close the modal
+          navigate("/"); // Navigate to the home page
+        }, 5000); // Close the modal after 5 seconds
       } else {
+        // If response is not okay, show the error message
         setError(data.error);
       }
     } catch (error) {
+      // If request fails for any reason, show an error message
       setError("An error occurred.");
     }
 
-    setLoading(false);
+    setLoading(false); // End loading
   };
 
   return (
@@ -57,15 +71,15 @@ function ConfirmationModal({ email, onClose }) {
           <input
             type="text"
             value={confirmationCode}
-            onChange={(e) => setConfirmationCode(e.target.value)}
+            onChange={(e) => setConfirmationCode(e.target.value)} // Update state with input field value
           />
         </label>
         <button type="submit">Confirm Email</button>
         <div className="spinner-container">
-          {loading && <div className="spinner"></div>}
+          {loading && <div className="spinner"></div>}{" "}
         </div>
-        {error && <div className="error">{error}</div>}
-        {message && <div className="message">{message}</div>}
+        {error && <div className="error">{error}</div>}{" "}
+        {message && <div className="message">{message}</div>}{" "}
       </form>
     </div>
   );
