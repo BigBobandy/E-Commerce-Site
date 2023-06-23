5243412a1653518c559a394e30c5d9a9567d8f8e
BigBobandy <jdykes207@gmail.com>
2023-06-23 17:21:50 -0400

    finally got my home page looking decent
    Also finally fixed the issue I was having with the white space on the bottom of the page.

--- front-end/src/components/Home-Components/HomePage.jsx
+++ front-end/src/components/Home-Components/HomePage.jsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import logoImg from "../../assets/borger-logo.png";
-import heroImg from "../../assets/hero-img.jpg";
 import "../styles/Home-Styles/HomePage.css";
 
 function HomePage() {
@@ -8,55 +7,61 @@ function HomePage() {
 
   return (
     <div className="home-container">
-      <section className="hero">
-        <div className="hero-text">
-          <h1>Welcome to Dirty burger!</h1>
-          <p>
-            Have you ever wanted a burger but you were too lazy to go to your
-            local fast food joint? Have you ever wished you had ordered a burger
-            a week ago that could be delivered by mail man to your doorstep in
-            your time of need? Here at Dirty Burger your days of worrying are
-            over. Place your order today and your future self will thank you!
-          </p>
-          <button onClick={() => navigate("/menu")} className="order-now-btn">
-            Order Now!
-          </button>
-        </div>
-        <div className="hero-img">
-          <img
-            src={heroImg}
-            alt="Wow that's a dirty burger bud"
-            className="hero-img"
-          />
+      <section className="hero-container">
+        <div className="hero-content">
+          <h1 className="title-text">Welcome to Dirty Burger!</h1>
+          <div className="hero-text">
+            <p>
+              Have you ever wanted a burger but you were too lazy to go to your
+              local fast food joint? Have you ever wished you had ordered a
+              burger 1-3 business days ago that could be delivered by mail man
+              to your doorstep in your time of need? Here at Dirty Burger your
+              days of worrying are over. Place your order today and your future
+              self will thank you!
+            </p>
+            <button onClick={() => navigate("/menu")} className="hero-button">
+              Order Now!
+            </button>
+            <h2>Become a member today</h2>
+            <p>
+              Our members enjoy discounts at checkout and free shipping on all
+              orders over $100.
+            </p>
+            <button onClick={() => navigate("/menu")} className="hero-button">
+              Sign Up!
+            </button>
+          </div>
         </div>
       </section>
       <section className="about-us-container">
-        <div className="about-left-side">
-          <h2>What is Dirty Burger?</h2>
-          <p>
-            We've got the greasiest, dirtiest burgers you'll ever shove into
-            your gut. You'll be a cheeseburger walrus in no time! Order today
-            and we can get your order from out kitchen to your doorstep in only
-            a matter of days maybe weeks. Whether it's cold outside or scorching
-            we will ship your favorite burgers, fries and shakes right to your
-            doorstep. Just pay shipping and handling!
-          </p>
-          <small className="disclaimer-text">
-            Disclaimer: The delivery trucks are not refrigerated. If
-            temperatures exceed 40°F your food may not be safe for consumption.
-            Eating spoiled fast food can cause food poisoning with symptoms such
-            as upset stomach, diarrhea and vomiting. There is also a risk of
-            botulism. Please consume at your own risk!
-          </small>
-          <h3>
-            We offer only the best product for our customers at Dirty Burger!
-            Your satisfaction is guaranteed!
-          </h3>
-          <small className="disclaimer-text">
-            Disclaimer: Your satisfaction is only guaranteed when delivery time
-            is estimated to be less than 10 - 12 hours and temperatures outside
-            are below 40°F.
-          </small>
+        <div className="about-us-content">
+          <div className="about-left-side">
+            <h2>What is Dirty Burger?</h2>
+            <p>
+              We've got the greasiest, dirtiest burgers you'll ever shove into
+              your gut. You'll be a cheeseburger walrus in no time! Order today
+              and we can get your order from out kitchen to your doorstep in
+              only a matter of days maybe weeks. Whether it's cold outside or
+              scorching we will ship your favorite burgers, fries and shakes
+              right to your doorstep. Just pay shipping and handling!
+            </p>
+            <small className="disclaimer-text">
+              Disclaimer: The delivery trucks are not refrigerated. If
+              temperatures exceed 40°F your food may not be safe for
+              consumption. Eating spoiled fast food can cause food poisoning
+              with symptoms such as upset stomach, diarrhea and vomiting. There
+              is also a risk of botulism. Please consume at your own risk!
+            </small>
+            <h3>
+              We offer only the best product for our customers at Dirty Burger!
+              Your satisfaction is guaranteed!
+            </h3>
+            <small className="disclaimer-text">
+              Disclaimer: Your satisfaction is only guaranteed when delivery
+              time is estimated to be less than 10 - 12 hours and temperatures
+              outside are below 40°F.
+            </small>
+          </div>
         </div>
         <div className="about-right-side">
           <img src={logoImg} alt="Dirty Burger Logo" className="logo-img" />
