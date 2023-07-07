0c8b1d103f36038863328f1e40180e8aba291685
BigBobandy <jdykes207@gmail.com>
2023-07-07 10:52:21 -0400

    Updated UserContext.jsx to handle and provide user shipping addresses.
    UserContext is now used to share not only the user's data, but also the user's addresses across components.
    
    The useState hook is now used to initialize a state variable addresses along with the setAddresses function to update it.
    
    The login function comment now clarifies that it stores the user's data and token in state and local storage.
    
    A logout function has been implemented which, alongside removing user data from state and clearing the token from local storage, also removes the addresses from state.
    
    The original useEffect hook that was used to validate the JWT when the application starts, now also fetches addresses for the user, if the JWT is valid. If the address response is ok, the addresses are extracted and the addresses state is updated.
    
    The UserContext.Provider now also provides addresses and setAddresses to its children. This means the children components can now access and update the addresses as needed.
    
    This is part of an ongoing effort to implement the shipping information feature in the application. The provided shipping info will be accessible by components that need it, like ShippingInfoModal in the UserProfile, and in future, the Checkout component once it's fully implemented.

--- front-end/src/components/User-Components/UserContext.jsx
+++ front-end/src/components/User-Components/UserContext.jsx
@@ -1,21 +1,22 @@
 import { createContext, useEffect, useState } from "react";
 
 // Using the `createContext` function from React to create a new context.
-// This context will be used to share the user's data across components.
+// This context will be used to share the user's data and addresses across components.
 const UserContext = createContext();
 
 // Creating a provider component for the UserContext.
-// This component will provide the user's data to all components inside of it.
+// This component will provide the user's data and addresses to all components inside of it.
 const UserProvider = ({ children }) => {
+  // Define state variables for user and addresses.
   const [user, setUser] = useState(null);
+  const [addresses, setAddresses] = useState([]);
 
   // Defining a function that logs the user in.
-  // This function accepts an object with the user's data and stores it in state.
-  // It also stores the user's JWT (JSON Web Token) in local storage.
+  // This function accepts an object with the user's data and token and stores it in state and local storage.
   const login = (userData) => {
     // Store user data in state
     setUser(userData);
-    // Store token in local storage
+    // Store token in local storage for persistence across sessions
     localStorage.setItem("token", userData.token);
   };
 
@@ -24,21 +25,21 @@ const UserProvider = ({ children }) => {
   const logout = () => {
     // Remove user data from state
     setUser(null);
-    // Remove token from local storage
+    // Remove addresses from state
+    setAddresses([]);
+    // Clear token from local storage
     localStorage.removeItem("token");
   };
 
-  // Using the `useEffect` hook to validate the JWT when the application starts.
-  // It does this by checking the local storage for a JSON web token that was stored when the user last logged in
-  // It then sends a fetch request to the server with the token found in the local storage
-  // The server will then attempt to validate the token
-  // If the JWT is valid, then update the user's data in state, logging them in, so that the user doesn't have to login again.
+  // Using the `useEffect` hook to validate the JWT and fetch addresses when the application starts.
+  // If the JWT is valid, the user's data will be updated in state, effectively logging them in.
   useEffect(() => {
-    const validateToken = async () => {
+    const initializeUserData = async () => {
       const token = localStorage.getItem("token");
 
       if (token) {
         try {
+          // Send fetch request to validate token
           const response = await fetch(
             "http://localhost:3000/api/login/validate-token",
             {
@@ -50,23 +51,51 @@ const UserProvider = ({ children }) => {
             }
           );
 
+          // Throw an error if response is not ok
           if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
           }
 
+          // If response is ok, extract user data and update user state
           const data = await response.json();
           setUser(data.user);
+
+          // Fetch existing addresses for the user
+          const addressResponse = await fetch(
+            "http://localhost:3000/api/shipping-info/get-shipping-info",
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+
+          // Throw an error if address response is not ok
+          if (!addressResponse.ok) {
+            throw new Error(`HTTP error! status: ${addressResponse.status}`);
+          }
+
+          // If address response is ok, extract addresses and update addresses state
+          const addressData = await addressResponse.json();
+          setAddresses(addressData);
         } catch (error) {
+          // Log any errors to console
           console.error(error);
         }
       }
     };
 
-    validateToken();
+    // Call function that validates a user JWT and fetches their address information if it exists
+    initializeUserData();
   }, []); // The empty array as a dependency means this effect will run once when the component mounts
 
+  // Provider component provides user and addresses state, login and logout functions to children
   return (
-    <UserContext.Provider value={{ user, setUser, login, logout }}>
+    <UserContext.Provider
+      value={{ user, setUser, login, logout, addresses, setAddresses }}
+    >
       {children}
     </UserContext.Provider>
   );
