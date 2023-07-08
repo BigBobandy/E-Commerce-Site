e6f3170ae037798b59a9047ed856928803e6aa21
BigBobandy <jdykes207@gmail.com>
2023-07-07 23:46:51 -0400

    Simplify error handling for rate limiting in EditProfileModal.jsx
    This commit addresses an issue with the handling of 429 (too many requests) error status codes in EditProfileModal.jsx.
    
    Previously, when a rate limit was hit, the front-end would calculate and display the time that the user could make requests again based on the server response. However, this was found to be problematic as the retry time was incorrectly calculated and updated based on the time of the last user request, not from when the actual timeout period would end.
    
    In this update, the error handling has been simplified. Instead of calculating and displaying a specific retry time, the application now displays a generic message advising the user that they have made too many requests and should try again in a few minutes.
    
    This change simplifies the user interface and removes the potential for confusion caused by incorrect retry time calculations.

--- front-end/src/components/User-Components/EditProfileModal.jsx
+++ front-end/src/components/User-Components/EditProfileModal.jsx
@@ -22,8 +22,12 @@ function EditProfileModal({ setIsEditModalOpen }) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
+  // Function to handle changing to a user's first and/or last name
   async function handleSubmit(e) {
     e.preventDefault();
+
+    // Clear any previous messages
+    setMessage("");
 
     // Input validation
     // Prevent the user from submitting changes without making changes to the name first
@@ -50,72 +54,56 @@ function EditProfileModal({ setIsEditModalOpen }) {
       lastName: capitalizeFirstLetter(newLastName),
     };
 
-    // get the JWT from local storage
-    const token = localStorage.getItem("token");
+    try {
+      // get the JWT from local storage
+      const token = localStorage.getItem("token");
 
-    const response = await fetch(
-      `http://localhost:3000/api/user/${user.userUrlString}/change-name`,
-      {
-        method: "PUT",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(updatedUser),
-      }
-    );
+      const response = await fetch(
+        `http://localhost:3000/api/user/${user.userUrlString}/change-name`,
+        {
+          method: "PUT",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(updatedUser),
+        }
+      );
 
-    // In case of a bad request
-    if (!response.ok) {
-      // If the status code is 429, it means the user has made too many requests
-      // Inform the user about the situation
-      if (response.status === 429) {
-        const responseData = await response.json();
-        const retryAfterTime = new Date(responseData.retryAfter);
-
-        // Convert the time to a more user-friendly format
-        let hours = retryAfterTime.getHours();
-        let minutes = retryAfterTime.getMinutes();
-        const ampm = hours >= 12 ? "pm" : "am";
-
-        // Convert to 12-hour format
-        hours = hours % 12;
-        // Convert "0" hour to "12" for the 12-hour clock
-        hours = hours ? hours : 12;
-
-        // Add leading zeros if necessary
-        minutes = minutes < 10 ? `0${minutes}` : minutes;
-
-        const retryTimeString = `${hours}:${minutes}${ampm}`;
-
-        setMessage(
-          `You have made too many requests. Please try again at ${retryTimeString}.`
-        );
+      // In case of a bad request
+      if (!response.ok) {
+        // If the status code is 429, it means the user has made too many requests
+        // Inform the user about the situation
+        if (response.status === 429) {
+          const responseData = await response.json();
+          setMessage(responseData.error);
+        } else {
+          // For other errors, the server should return a message in case of an error
+          // This message will be displayed to the user
+          const errorMessage = await response.json();
+          throw new Error(errorMessage);
+        }
       } else {
-        // For other errors, the server should return a message in case of an error
-        // This message will be displayed to the user
-        const errorMessage = await response.json();
-        console.log(errorMessage);
-        setMessage(`Error updating user: ${errorMessage.error}`);
+        const updatedUser = await response.json();
+
+        // If response is okay clear any previous error messages
+        setMessage("");
+
+        // After successfully updating the user, update the user in the UserContext.
+        // This assumes you have a setUser function in your UserContext.
+        setUser(updatedUser);
+
+        // Close the edit modal after success
+        setIsEditModalOpen(false);
+
+        // Display a success message
+        setMessage("User updated successfully!");
+
+        // Forcing a page reload here so that the new user information is displayed correctly
+        window.location.reload();
       }
-    } else {
-      const updatedUser = await response.json();
-
-      // If response is okay clear any previous error messages
-      setMessage("");
-
-      // After successfully updating the user, update the user in the UserContext.
-      // This assumes you have a setUser function in your UserContext.
-      setUser(updatedUser);
-
-      // Close the edit modal after success
-      setIsEditModalOpen(false);
-
-      // Display a success message
-      setMessage("User updated successfully!");
-
-      // Forcing a page reload here so that the new user information is displayed correctly
-      window.location.reload();
+    } catch (error) {
+      setMessage(error.message);
     }
   }
 
