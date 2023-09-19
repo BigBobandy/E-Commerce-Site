e9fb8f48e31c005c11e72e688abfd41829603bda
BigBobandy <jdykes207@gmail.com>
2023-09-19 11:31:12 -0400

    Introduced ShippingMethods component for shipping option selection.
    The ShippingMethods component is part of the CheckoutDetails and is responsible for allowing users to select a shipping method. It offers multiple shipping options like "Standard," "Express," and "Overnight," along with a "Free" option for orders over $100. The component uses a useEffect to set a default shipping method based on the user's eligibility for free shipping and the total cost of the cart. Users can change their shipping method through radio buttons, and the shipping cost is dynamically calculated and updated using the calculateShippingPrice function. Messages are displayed to inform users about free shipping eligibility. I'm pretty sure this component will work without requiring a user to be logged in but I will need to do some more testing on it when I return to fix it's siblings the Billing and Shipping components later.

---
+++ front-end/src/components/Checkout-Components/ShippingMethods.jsx
@@ -0,0 +1,143 @@
+import { useContext, useEffect } from "react";
+import "../../styles/Checkout-Styles/ShippingMethods.css";
+import { UserContext } from "../User-Components/UserContext";
+
+function ShippingMethods({
+  totalCost,
+  setShippingCost,
+  shippingMethod,
+  setShippingMethod,
+}) {
+  const { user } = useContext(UserContext);
+
+  // UseEffect hook for selecting a default shipping method
+  useEffect(() => {
+    // Initialize the default shipping method
+    let defaultMethod = "standard"; // Default to standard shipping
+    if (user && totalCost >= 100) {
+      defaultMethod = "free"; // If user qualifies for free shipping
+    }
+    setShippingMethod(defaultMethod);
+    setShippingCost(calculateShippingPrice(defaultMethod));
+  }, [totalCost, user]);
+
+  // This function handles the change in shipping method selected by the user.
+  const handleShippingMethodChange = (e) => {
+    // Get the value of the selected shipping method from the radio button.
+    const selectedShippingMethod = e.target.value;
+
+    // Update the state to reflect the new selected shipping method.
+    setShippingMethod(selectedShippingMethod);
+
+    // Calculate and update the shipping cost based on the selected method.
+    setShippingCost(calculateShippingPrice(selectedShippingMethod));
+
+    // Log the current shipping method for debugging purposes.
+    console.log(shippingMethod);
+  };
+
+  // This function calculates the shipping price based on the selected method.
+  const calculateShippingPrice = (method) => {
+    // Use a switch statement to determine the shipping cost.
+    switch (method) {
+      case "standard":
+        return 15.99;
+      case "express":
+        return 22.99;
+      case "overnight":
+        return 34.99;
+      case "free":
+        return 0;
+      default:
+        return 0;
+    }
+  };
+
+  return (
+    <div className="shipping-methods-details-container">
+      <div className="shipping-methods-header">
+        <div>
+          <h2 className="checkout-step-counter">3</h2>
+        </div>
+        <div>
+          <h3>Select Shipping Method</h3>
+        </div>
+      </div>
+      <div className="radio-options-container">
+        {user && totalCost < 100 && (
+          <div className="message-wrapper">
+            <p className="shipping-methods-message">
+              You're so close to qualifying for free shipping! Our members enjoy
+              free shipping on all orders of $100 or more.
+            </p>
+          </div>
+        )}
+        {user && totalCost >= 100 && (
+          <div className="shipping-method-option">
+            <div className="message-wrapper">
+              <p className="shipping-methods-message">
+                Your order is $100 or more you qualify for free shipping!
+              </p>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="free"
+                name="shipping"
+                value="free"
+                className="styled-radio-button"
+                checked={shippingMethod === "free"}
+                onChange={handleShippingMethodChange}
+              />
+              <label htmlFor="free">Member Shipping (7-10 days) - FREE</label>
+            </div>
+          </div>
+        )}
+        <div className="shipping-method-option">
+          <div>
+            <input
+              type="radio"
+              id="standard"
+              name="shipping"
+              value="standard"
+              className="styled-radio-button"
+              checked={shippingMethod === "standard"}
+              onChange={handleShippingMethodChange}
+            />
+            <label htmlFor="standard">Standard (5-7 days) - $15.99</label>
+          </div>
+        </div>
+        <div className="shipping-method-option">
+          <div>
+            <input
+              type="radio"
+              id="express"
+              name="shipping"
+              value="express"
+              className="styled-radio-button"
+              checked={shippingMethod === "express"}
+              onChange={handleShippingMethodChange}
+            />
+            <label htmlFor="express">Express (2-3 days) - $22.99</label>
+          </div>
+        </div>
+        <div className="shipping-method-option">
+          <div>
+            <input
+              type="radio"
+              id="overnight"
+              name="shipping"
+              value="overnight"
+              className="styled-radio-button"
+              checked={shippingMethod === "overnight"}
+              onChange={handleShippingMethodChange}
+            />
+            <label htmlFor="overnight">Overnight (1 day) - $34.99</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ShippingMethods;
