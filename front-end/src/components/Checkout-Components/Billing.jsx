fe96f5838cd2ba6ca703d12ad64b62f8f29641fd
BigBobandy <jdykes207@gmail.com>
2023-09-19 11:28:10 -0400

    Added Billing component for payment method selection and management.
    The Billing component is designed to handle the user's payment method during the checkout process. It fetches existing card information from the UserContext and allows users to add new cards through a modal. The component features an expandCards state to switch between a summary and detailed view of payment methods. A useEffect sets a default card if one exists. Users can select their payment method through radio buttons, and new cards can be added via the "Add new card" button, which opens the CreateCardModal which is the same one I used in the BillingInfoModal. As it stands right now, just like the Shipping.jxs component, this component only works if a user is both logged in, and has some card information already stored for them. I will need to return to this later and fix it!

---
+++ front-end/src/components/Checkout-Components/Billing.jsx
@@ -0,0 +1,140 @@
+import { useContext, useEffect, useState } from "react";
+import { getLastFourDigits } from "../../helpers/cardHelper";
+import CreateCardModal from "../Modal-Components/CreateCardModal";
+import { UserContext } from "../User-Components/UserContext";
+
+function Billing({
+  userFirstName,
+  userLastName,
+  expandCards,
+  setExpandCards,
+  setExpandAddresses,
+  cardSelection,
+  setCardSelection,
+}) {
+  const { cardInfo, setCardInfo } = useContext(UserContext);
+  const [isCreateCardModalOpen, setIsCreateCardModalOpen] = useState(false);
+
+  useEffect(() => {
+    // Check if cardInfo exists
+    if (cardInfo && cardInfo.length > 0) {
+      // Search for a default card in the cardInfo array
+      const defaultCard = cardInfo.find((card) => card.isDefault);
+
+      // If a default card is foiund, set it as the cardSelection
+      if (defaultCard) {
+        setCardSelection(defaultCard);
+      }
+    }
+  }, [cardInfo]);
+
+  // Function to handle selecting a payment method or in other words a card
+  const handleCardSelection = (cardIndex) => {
+    setCardSelection(cardInfo[cardIndex]);
+  };
+
+  return (
+    <div>
+      {cardSelection ? (
+        <>
+          {!expandCards ? (
+            <div className="checkout-details-container">
+              <div>
+                <h2 className="checkout-step-counter">2</h2>
+              </div>
+              <div>
+                <h3>Payment Method</h3>
+              </div>
+              <div className="checkout-detail-container">
+                <p className="detail-choice-name">{cardSelection.cardHolder}</p>
+                <p>
+                  {cardSelection.cardType} Ending in{" "}
+                  {getLastFourDigits(cardSelection.cardNumber)}
+                </p>
+                <p>Expires: {cardSelection.expiryDate}</p>
+              </div>
+              <div>
+                <button
+                  className="change-address-btn"
+                  onClick={() => {
+                    setExpandCards(true);
+                    setExpandAddresses(false);
+                  }}
+                >
+                  Change
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="detail-selection-container">
+              <div className="detail-header-container">
+                <h2 className="checkout-step-counter">2</h2>
+                <h3>Choose a payment method</h3>
+                <button
+                  className="change-address-btn go-back-btn"
+                  onClick={() => setExpandCards(false)}
+                >
+                  Close X
+                </button>
+              </div>
+              <div className="card-info-details-container">
+                <h4 className="checkout-step-counter">Your cards:</h4>
+                <h4>Name on Card</h4>
+                <h4>Expires On</h4>
+              </div>
+              <div className="detail-choice">
+                {cardInfo.map((card, index) => (
+                  <div key={index} className="card-choice-item">
+                    <div className="checkout-input-label-container">
+                      <input
+                        type="radio"
+                        id={`card${index}`}
+                        name="cardSelection"
+                        className="card-radio-btn"
+                        checked={cardSelection === card}
+                        onChange={() => handleCardSelection(index)}
+                      />
+                      <label
+                        htmlFor={`card${index}`}
+                        className="card-choice-label"
+                      >
+                        {card.cardType} Ending in{" "}
+                        {getLastFourDigits(card.cardNumber)}
+                      </label>
+                    </div>
+                    <div className="card-choice-details">
+                      <p>{card.cardHolder}</p>
+                      <p>{card.expiryDate}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <button
+                className="add-detail-btn"
+                onClick={() => setIsCreateCardModalOpen(true)}
+              >
+                <span>+</span> Add new card
+              </button>
+            </div>
+          )}
+        </>
+      ) : (
+        <button onClick={() => setIsCreateCardModalOpen(true)}>
+          Add New Card
+        </button>
+      )}
+      {isCreateCardModalOpen && (
+        <div className="animation">
+          <CreateCardModal
+            setIsCreateCardModalOpen={setIsCreateCardModalOpen}
+            userFirstName={userFirstName}
+            userLastName={userLastName}
+            setCardInfo={setCardInfo}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Billing;
