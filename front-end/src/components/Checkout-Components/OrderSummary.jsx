070c66e93d9734aee86d5e3889081950e9acadd6
BigBobandy <jdykes207@gmail.com>
2023-10-06 12:52:10 -0400

    Updated OrderSummary component.
    Added new state variables deliveryDate, isLoading, and message.
    Introduced a function getDeliveryDate to calculate the expected delivery date based on the selected shipping method.
    Implemented handleSubmitOrder function to handle the order submission process, including validations and API calls.
    Added logic to update the cart and order status after a successful order placement.
    Included a loading spinner and success message after placing an order.
    Modified the UI to conditionally render buttons and messages based on the order placement status.

--- front-end/src/components/Checkout-Components/OrderSummary.jsx
+++ front-end/src/components/Checkout-Components/OrderSummary.jsx
@@ -11,17 +11,17 @@ function OrderSummary({
   shippingMethod,
   cardSelection,
   addressSelection,
-  user,
+  cart,
+  setCart,
+  isOrderPlaced,
+  setIsOrderPlaced,
+  setOrderDetailsState,
 }) {
   const [taxAmount, setTaxAmount] = useState(0);
   const [orderTotal, setOrderTotal] = useState(0);
-
-  console.log(
-    "Checkout Details:",
-    shippingMethod,
-    cardSelection,
-    addressSelection
-  );
+  const [deliveryDate, setDeliveryDate] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
   // Calculate the tax amount based on the total cost
   const calculateTaxAmount = (totalCost) => {
@@ -30,11 +30,139 @@ function OrderSummary({
     return totalCost * taxRate;
   };
 
+  // Function to calculate the expected delivery date
+  const getDeliveryDate = (shippingMethod) => {
+    const currentDate = new Date();
+    let deliveryDays;
+
+    console.log(shippingMethod);
+
+    switch (shippingMethod) {
+      case "free":
+        deliveryDays = 10;
+        break;
+      case "standard":
+        deliveryDays = 7;
+        break;
+      case "express":
+        deliveryDays = 3;
+        break;
+      case "overnight":
+        deliveryDays = 1;
+        break;
+      default:
+        deliveryDays = 0;
+    }
+
+    currentDate.setDate(currentDate.getDate() + deliveryDays);
+    return currentDate.toDateString();
+  };
+
   // Update the tax amount and order total when the total cost changes
   useEffect(() => {
     setTaxAmount(calculateTaxAmount(totalCost));
     setOrderTotal(totalCost + shippingCost + calculateTaxAmount(totalCost));
   }, [totalCost, shippingCost]);
+
+  // Handles sending the request to create an order
+  async function handleSubmitOrder() {
+    // Clear any previous messages
+    setMessage("");
+
+    // Validate if a shipping address is selected
+    if (!addressSelection || !addressSelection.id) {
+      setMessage("Please select a shipping address.");
+      setIsLoading(false);
+      return;
+    }
+
+    // Validate if a billing method is selected
+    if (!cardSelection || !cardSelection.id) {
+      setMessage("Please select a billing method.");
+      setIsLoading(false);
+      return;
+    }
+
+    // Validate if a shipping method is selected
+    if (!shippingMethod) {
+      setMessage("Please select a shipping method.");
+      setIsLoading(false);
+      return;
+    }
+
+    // Set loading state to true
+    setIsLoading(true);
+
+    // Calculate and set the expected delivery date
+    const calculatedDeliveryDate = getDeliveryDate(shippingMethod);
+
+    // Prepare order details object for POST request
+    // I just want to send the id of the card and address for the post request as this is sensitive information
+    const orderDetailsForPost = {
+      orderItems: cart,
+      shippingInfoId: addressSelection.id,
+      cardInfoId: cardSelection.id,
+      totalPrice: orderTotal,
+      shippingMethod,
+      shippingCost,
+      deliveryDate: calculatedDeliveryDate,
+    };
+
+    // Prepare order details object for local state
+    // Seperate order details object specifically to be used for the order confirmation component
+    // It has the full address and card details of the selected card instead of just the ids
+    const orderDetailsForState = {
+      ...orderDetailsForPost,
+      shippingInfo: addressSelection, // Full address details
+      cardInfo: cardSelection, // Full card details
+    };
+
+    // Update state with full order details
+    setOrderDetailsState(orderDetailsForState);
+
+    // Retrieve token from local storage
+    const token = localStorage.getItem("token");
+
+    try {
+      // Send POST request to submit the order
+      const response = await fetch("http://localhost:3000/api/orders/submit", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderDetailsForPost),
+      });
+
+      // Handle unsuccessful responses
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error);
+      }
+
+      // Handle successful response
+      const data = await response.json();
+      console.log("Order submitted successfully:", data);
+
+      // Update the orderDetailsState with the order number from the response
+      setOrderDetailsState((prevOrderDetails) => ({
+        ...prevOrderDetails,
+        orderNumber: data.orderNumber,
+      }));
+
+      // Reset loading state and set order as placed
+      setMessage("Order Placed Successfully!");
+      setIsLoading(false);
+      setIsOrderPlaced(true);
+      setCart([]);
+
+      // Redirect to a confirmation page or perform other actions
+    } catch (error) {
+      // Handle errors during the fetch
+      console.error("Failed to submit order:", error);
+      setIsLoading(false);
+    }
+  }
 
   return (
     <div className="order-summary-container">
@@ -63,13 +191,32 @@ function OrderSummary({
               <h3 className="total-title">Order total:</h3>
               <h3>${orderTotal.toFixed(2)}</h3>
             </div>
+            {!isOrderPlaced && (
+              <div className="message-wrapper">
+                <h4>{message}</h4>
+              </div>
+            )}
           </>
         )}
       </div>
       <div className="message-wrapper">
-        <button onClick={goToNextStage} className="checkout-button">
-          {getNextStage(currentStage)}
-        </button>
+        {currentStage === "Shopping Cart" ? (
+          <button onClick={goToNextStage} className="checkout-button">
+            {getNextStage(currentStage)}
+          </button>
+        ) : (
+          <>
+            {isLoading ? (
+              <div className="spinner"></div>
+            ) : isOrderPlaced ? (
+              <h4>Order has been placed successfully.</h4>
+            ) : (
+              <button onClick={handleSubmitOrder} className="checkout-button">
+                Place Order
+              </button>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
