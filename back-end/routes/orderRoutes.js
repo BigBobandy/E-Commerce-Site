3d2802d3d861fae68654ea7e1fe3165c84fcb795
BigBobandy <jdykes207@gmail.com>
2023-10-09 10:11:32 -0400

    added a route for get requests for order info

--- back-end/routes/orderRoutes.js
+++ back-end/routes/orderRoutes.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { submitOrder } = require("../controllers/orderControllers");
+const {
+  submitOrder,
+  getOrderInfo,
+} = require("../controllers/orderControllers");
 
 router.post("/submit", submitOrder);
+
+router.get("/get", getOrderInfo);
 
 module.exports = router;
